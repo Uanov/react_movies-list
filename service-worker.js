0a1af1a690404d22124b3459998a8a9fd2029798
Artem Maksimikhin <uanov86@gmail.com>
2021-03-30 21:59:27 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.8cca011b1840c2dbe523532ff377bafa.js"
 );
 
 self.addEventListener('message', (event) => {
